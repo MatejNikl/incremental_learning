edffc1652ec6d559e680c2869952b61ba9c2997a
Matej Nikl <niklmate@fit.cvut.cz>
2016-08-26 02:13:48 +0200

    Add early stopping

--- task/train.lua
+++ task/train.lua
@@ -25,10 +25,17 @@ local function parse_args(args)
    }
 
    op:option{
-      "--visual-check",
-      action = "store_true",
-      dest   = "visual_check",
-      help   = "after training show input images one by one + the net's responses",
+      "--split",
+      default = 0.8,
+      dest    = "split",
+      help    = "keep this percentage for training and the rest for validation"
+   }
+
+   op:option{
+      "--try-epochs",
+      default = 5,
+      dest    = "try_epochs",
+      help    = "keep trying for # epochs to find better parameters (early stopping)",
    }
 
    op:option{
@@ -102,10 +109,10 @@ local function parse_args(args)
    }
 
    op:option{
-      "--split",
-      default = 0.8,
-      dest    = "split",
-      help    = "keep this percentage for training and the rest for validation"
+      "--visual-check",
+      action = "store_true",
+      dest   = "visual_check",
+      help   = "after training show input images one by one + the net's responses",
    }
 
 
@@ -123,6 +130,7 @@ local function parse_args(args)
    opts.dropout     = tonumber(opts.dropout)
    opts.dropconnect = tonumber(opts.dropconnect)
    opts.split       = tonumber(opts.split)
+   opts.try_epochs  = tonumber(opts.try_epochs)
    opts.layers      = loadstring("return {" .. opts.layers .. "}")()
 
    return opts, args
@@ -175,12 +183,13 @@ local create_log = argcheck{
             'valid_loss',
             'valid_acc',
             'epoch_time',
+            'new_best',
          }
 
          local logtext   = require 'torchnet.log.view.text'
          local logstatus = require 'torchnet.log.view.status'
 
-         local format  = {'%4d', lossfmt, accfmt, lossfmt, accfmt, '%7.3fs'}
+         local format = {'%4d', lossfmt, accfmt, lossfmt, accfmt, '%7.3fs', '%s'}
 
          return tnt.Log{
             keys = logkeys,
@@ -230,6 +239,25 @@ local visualize_layer = argcheck{
       end
 }
 
+local best_acc = -math.huge
+local best_net
+local epochs_waited
+local early_stopping = argcheck{
+   {name='current_acc', type='number'},
+   {name='net', type='nn.Sequential'},
+   call =
+      function(current_acc, net)
+         if current_acc > best_acc then
+            best_acc = current_acc
+            best_net = net:clone():clearState()
+            epochs_waited = 0
+         else
+            epochs_waited = epochs_waited + 1
+         end
+
+         return epochs_waited
+      end
+}
 
 sig.signal(sig.SIGINT, sig.signal_handler)
 
@@ -302,10 +330,13 @@ engine.hooks.onEndEpoch = function(state)
    }
    train_dataset:select('train')
 
+   local epochs_waited = early_stopping(mapmeter:value(), state.network)
+
    log:set{
       valid_loss = avgloss:value(),
       valid_acc  = mapmeter:value() * 100,
       epoch_time = timer:value(),
+      new_best   = epochs_waited == 0 and '<--' or ''
    }
    log:flush()
 
@@ -314,14 +345,13 @@ engine.hooks.onEndEpoch = function(state)
       visualize_window = visualize_layer(state.network.modules, visualize_window)
    end
 
-   if _G.interrupted then
+   if epochs_waited == opts.try_epochs or _G.interrupted then
       if visualize_window then visualize_window.window:close() end
       state.maxepoch = 0 -- end training
    end
 
    state.iterator:exec('resample') -- call :resample() on the underlying dataset
 end
-
 
 if opts.train_path then
    -- train the model:
@@ -334,10 +364,13 @@ if opts.train_path then
    }
 end
 
+net = nil -- use best_net instead
+
+
 if opts.test_path then
    -- measure test loss and error:
    engine:test{
-      network   = net,
+      network   = best_net,
       iterator  = test_dataset:batch(test_dataset:size()):iterator(),
       criterion = criterion,
    }
@@ -351,7 +384,7 @@ if opts.test_path then
       _G.interrupted = nil
       for data in test_dataset:iterator()() do
          w = image.display{image=data.input:view(1, 64, 64), win = w}
-         local a = net:forward(data.input):squeeze()
+         local a = best_net:forward(data.input):squeeze()
          local b = data.target
          a = torch.cat(a, a:ge(0.5):double(), 2)
          print(torch.cat(a, b, 2):t())
@@ -367,6 +400,6 @@ if opts.test_path then
 end
 
 if #args > 0 then
-   torch.save(args[1], net:clearState())
-   print("Saved the trained network as '" .. args[1] .. "'")
+   torch.save(args[1], best_net)
+   print("Saved the best trained network as '" .. args[1] .. "'")
 end
